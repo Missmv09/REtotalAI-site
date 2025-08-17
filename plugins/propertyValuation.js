1fe63da63797716f60c574088644887e6247ca1a
Missmv09 <missmv@gmail.com>
2025-08-16 22:38:08 -0700

    Add property details fetch and auto-population

--- plugins/propertyValuation.js
+++ plugins/propertyValuation.js
@@ -7,3 +7,13 @@ export async function getPropertyValuation({ address, city, state, zipcode, prop
   }
   return response.json();
 }
+
+export async function getPropertyDetails({ address, city, state, zipcode }) {
+  const params = new URLSearchParams({ address, city, state, zipcode });
+  const url = `https://api.externalpropertyvaluation.com/details?${params.toString()}`;
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error('Failed to fetch property details');
+  }
+  return response.json();
+}
